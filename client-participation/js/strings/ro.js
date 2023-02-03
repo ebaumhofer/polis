28a05cef34175dbd78de8a95ffc8d3712f860b8d
ebaumhofer <51317950+ebaumhofer@users.noreply.github.com>
2023-02-03 12:35:06 +0100

    Create ro.js

---
+++ client-participation/js/strings/ro.js
@@ -0,0 +1,123 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var s = {};
+
+s.agree = "Agree"; „De acord”
+s.disagree = "Disagree"; „Nu sunt de acord”
+s.pass = "Pass / Unsure"; „Nu știu / Nu sunt sigur”
+
+s.importantCheckbox = "This comment is important"; „Acest comentariu este important”
+s.howImportantPrompt = "How important is this statement?"; „Cât de importantă este această afirmație?”
+s.howImportantLow = "Low"; „Puțin importantă”
+s.howImportantMedium = "Medium"; „Oarecum importantă”
+s.howImportantHigh = "High"; „Foarte importantă”
+ 
+s.modSpam = "Spam"; „Spam”
+s.modOffTopic = "Off Topic"; „Off Topic / Nu este la subiectul discuției”
+s.modImportant = "Important"; „Important”
+s.modSubmitInitialState = "Skip (none of the above), next statement"; „Ignorează (nici una de mai sus), următoarea afirmație”
+s.modSubmit = "Done, next statement"; „Bifat, următoarea afirmație”
+ 
+s.x_wrote = "wrote:"; „A scris:”
+s.x_tweeted = "tweeted:"; „A postat”
+s.comments_remaining = "{{num_comments}} remaining"; „{{num_comments}} rămase”
+s.comments_remaining2 = "{{num_comments}} remaining statements"; „{{num_comments}} afirmații rămase”
+s.group_123 = "Group:"; „Grup:”
+s.comment_123 = "Statement:"; „Afirmație:”
+s.majorityOpinion = "Majority Opinion"; „Opinia majorității”
+s.majorityOpinionShort = "Majority"; „Majoritatea”
+s.info = "Info"; „Informație”
+s.addPolisToYourSite = "<img style='height: 20px; margin: 0px 4px;' src='{{URL}}'/>";
+s.privacy = "Privacy"; „Politică de confidențialitate”
+s.TOS = "TOS"; „Termeni de utilizare”
+s.writePrompt = "Share your perspective..."; „Împărtășește-ți viziunea”
+s.anonPerson = "Anonymous"; „Anonim”
+s.helpWhatAmISeeingTitle = "What am I seeing?"; „Ce  văd?”
+s.helpWhatAmISeeing = "People who vote similarly are grouped. Click a group to see which viewpoints they share."; „Oamenii care votează similar sunt grupați. Faceți click pe un grup pentru a vedea ce punct de vedere împărtășesc”
+s.helpWhatDoIDoTitle = " What do I do?"; „Ce să fac?”
+s.helpWhatDoIDo = "Vote on other people's statements by clicking 'agree' or 'disagree'. Write a statement (keep each to a single idea). Invite your friends to the conversation!"; „Votează pentru afirmațiile altor persoane făcând click pe butoanele „de acord” sau „nu sunt de acord”. Scrie un comentariu (un comentariu trebuie să conțină o singură idee). Invită prietenii să se alăture discuției!”
+s.writeCommentHelpText = "Are your perspectives or experiences missing from the conversation? If so, <b>add them</b> in the box below."; „Viziunea sau experiența ta lipsește în această discuție? Dacă da <b> scrie ce crezi </b> în câmpul de mai jos.”
+s.helpWriteListIntro = "What makes a good statement?"; „Cum să scrii un comentariu bun?”
+s.helpWriteListStandalone = "Stand alone idea"; „Notează o singură idee”
+s.helpWriteListRaisNew = "Raise new perspectives, experiences or issues"; „Abordează viziuni, experiențe sau probleme noi”
+s.helpWriteListShort = "Clear & concise (limited to 140 characters)"; „Scrie clar și concis (până la 140 caractere)”
+s.heresHowGroupVoted = "Here's how Group {{GROUP_NUMBER}} voted:"; „Vezi cum a votat Grupul {{GROUP_NUMBER}} ”
+s.one_person = "{{x}} person"; „persoană”
+s.x_people = "{{x}} people"; „persoane”
+s.acrossAllPtpts = "Across all participants:" ; „Din total participanți”
+s.xPtptsSawThisComment = " saw this statement"; „Am văzut această afirmație”
+s.xOfThoseAgreed = "of those participants agreed"; „din participanți au fost de acord”
+s.xOfthoseDisagreed = "of those participants disagreed"; „din participanți nu au fost de acord”
+s.opinionGroups = "Opinion Groups"; „Grupuri de opinii”
+s.topComments = "Top Statements"; „Cele mai populare comentarii”
+s.divisiveComments = "Divisive Statements"; „Afirmații care dezbină”
+s.pctAgreed = "{{pct}}% Agreed"; "{{pct}}% Au fost de acord"
+s.pctDisagreed = "{{pct}}% Disagreed"; "{{pct}}% Nu au fost de acord"
+s.pctAgreedLong = "{{pct}}% of everyone who voted on statement {{comment_id}} agreed."; "{{pct}}% din cei care au votat la această afirmație {{comment_id}} au fost de acord."
+s.pctAgreedOfGroup = "{{pct}}% of Group {{group}} Agreed"; "{{pct}}% din Grup {{group}} au fost de acord"
+s.pctDisagreedOfGroup = "{{pct}}% of Group {{group}} Disagreed"; "{{pct}}% din Group {{group}} nu au fost de acord"
+s.pctDisagreedLong = "{{pct}}% of everyone who voted on statement {{comment_id}} disagreed.";  "{{pct}}% din cei care au votat la această afirmație {{comment_id}} nu a fost de acord."
+s.pctAgreedOfGroupLong = "{{pct}}% of those in group {{group}} who voted on statement {{comment_id}} agreed.";  "{{pct}}%din cei din grupul {{group}} care au votat la afirmația {{comment_id}} au fost de acord."
+s.pctDisagreedOfGroupLong = "{{pct}}% of those in group {{group}} who voted on statement {{comment_id}} disagreed."; "{{pct}}%din cei din grupul {{group}} care au votat la afirmația {{comment_id}} nu au fost de acord."
+s.commentSent = "Statement submitted! Only other participants will see your statement and agree or disagree."; „Afirmația a fost trimisă! Doar alți participanți o vor putea vedea și vota de acord sau nu sunt de acord”
+s.commentSendFailed = "There was an error submitting your statement."; „Eroare. Afirmația ta nu a fost trimisă.”
+s.commentSendFailedEmpty = "There was an error submitting your statement - Statement should not be empty."; „Afirmația ta nu a fost trimisă din cauza unei erori. Nu poți trimite mesaj fără text.”
+s.commentSendFailedTooLong = "There was an error submitting your statement - Statement is too long."; „Afirmația ta nu a fost trimisă din cauza unei erori - ai depășit limita de caractere.”
+s.commentSendFailedDuplicate = "There was an error submitting your statement - An identical statement already exists."; „Afirmația ta nu a fost trimisă din cauza unei erori - deja există o afirmație identică”
+s.commentErrorDuplicate = "Duplicate! That statement already exists."; „Această afirmație deja există!”
+s.commentErrorConversationClosed = "This conversation is closed. No further statements can be submitted."; „Această discuție a fost închisă. Nu mai pot fi trimise afirmații.”
+s.commentIsEmpty = "Statement is empty"; „Acest mesaj nu conține text”
+s.commentIsTooLong = "Statement is too long"; „Ai depășit limita de caractere”
+s.hereIsNextStatement = "Vote success. Navigate up to see the next statement."; „Ai votat cu succes. Vezi mai sus următoarele comentarii.”
+
+s.connectFacebook = "Connect Facebook"; „Loghează-te cu cont Facebook”
+s.connectTwitter = "Connect Twitter"; „Loghează-te cu cont Twitter”
+s.connectToPostPrompt = "Connect an identity to submit a statement. We will not post to your timeline."; „Loghează-te pentru a putea trimite un comentariu. Nu vom posta nimic pe rețelele tale sociale.”
+s.connectToVotePrompt = "Connect an identity to vote. We will not post to your timeline."; „Loghează-te pentru a putea vota. Nu vom posta nimic pe rețelele tale sociale.”
+s.tip = "Tip:"; „Sugestie”
+s.commentWritingTipsHintsHeader = "Tips for writing statements"; „Sugestii pentru a scrie afirmații”
+s.tipCharLimit = "Statements are limited to {{char_limit}} characters."; "Comentariile sunt limitate la {{char_limit}} caractere."
+s.tipCommentsRandom = "Please remember, statements are displayed randomly and you are not replying directly to other participants' statements."; „Atenție: comentariile sunt afișate în ordine aleatorie. Nu poți răspunde la afirmațiile altor participanți.”
+s.tipOneIdea = "Break up long statements that contain multiple ideas. This makes it easier for others to vote on your statement."; „Afirmațiile lungi care conțin mai multe idei trebuie împărțite în câteva comentarii, pentru ca alți participanți să poată vota pentru ele.”
+s.tipNoQuestions = "Statements should not be in the form of a question. Participants will agree or disagree with the statements you make."; „Comentariile nu pot conține întrebări. Participanții urmează să voteze de acord sau nu sunt de acord cu afirmația ta.”
+s.commentTooLongByChars = "Statement length limit exceeded by {{CHARACTERS_COUNT}} characters."; „Comentariu prea lung. Ai depășit limita cu {{CHARACTERS_COUNT}} caractere.”
+s.notSentSinceDemo = "(not really, this is a demo)"; „(nu tocmai, este o demonstrație)”
+s.submitComment = "Submit"; „Trimite”
+s.tipStarred = "Marked as important."; „Important”
+s.participantHelpWelcomeText = "Welcome to a new kind of conversation - <em>vote</em> on other people's statements.";  "Încearcă o nouă experiență de discuție - <em>vote</em> comentează afirmațiile altor participanți."
+"s.participantHelpGroupsText = "People who vote similarly <span style='font-weight: 700;'>are grouped.</span> Click a group to see which viewpoints they share. <a style='font-weight: 700; cursor: pointer; text-decoration: underline' id='helpTextGroupsExpand'>...more</a>"; "Oamenii care votează similar <span style='font-weight: 700;'> sunt grupați.</span> Fă click pe un grup  ca să vezi ce viziuni împărtășesc. <a style='font-weight: 700; cursor: pointer; text-decoration: underline' id='helpTextGroupsExpand'>...more</a>.
+s.participantHelpGroupsNotYetText = "The visualization will appear once 7 participants have begun voting"; „Vizualizarea va fi disponibilă după ce vor vota cel puțin șapte participanți.”
+s.helpWhatAreGroupsDetail = "<p>You've probably seen 'recommended products' on Amazon, or 'recommended movies' on Netflix. Each of those services uses statistics to group the user with people who buy and watch similar things, then show them things that those people bought or watched.</p> <p> When a user votes on statements, they are grouped with people who voted like they did! You can see those groups below. Each is made up of people who have similar opinions. There are fascinating insights to discover in each conversation. Go ahead - click a group to see what brought them together and what makes them unique! </p>"; "<p>Probabil ai văzut „produse recomandate” pe Amazon, sau „filme recomandate” pe Netflix. Aceste service-uri folosesc statistici pentru a grupa utilizatorii care cumpără produse similare sau privesc aceleași filme ca să-ți arate ce cumpără sau privesc alți oameni..</p> <p> Când un utilizator votează comentariile este grupat cu alții care au votat la fel! Mai jos poți vedea aceste grupuri. Fiecare grup conține oameni cu opinii similare. Sunt lucruri fascinante de descoperit în fiecare discuție. Vezi - fă click pe un grup de oameni și vezi ce au în comun și ce îi deosebește de alții! </p>"
+s.socialConnectPrompt = "Optionally connect to see friends and people you follow in the visualization."; „Conectează opțiunea pentru a putea vedea în vizualizare prietenii sau persoanele la care ești abonat în rețele sociale.”
+s.connectFbButton = "Connect with Facebook"; ”Loghează-te cu Facebook”
+s.connectTwButton = "Connect with Twitter";”Loghează-te cu Twitter”
+s.polis_err_reg_fb_verification_email_sent = "Please check your email for a verification link, then return here to continue."; ”Verifică-ți email-ul pentru link-ul de verificare, apoi revină pentru a continua”
+s.polis_err_reg_fb_verification_noemail_unverified = "Your Facebook account is unverified. Please verify your email address with Facebook, then return here to continue.";”Contul tău de Facebook nu este verificat.Verifică adresa de email cu Facebook și revină pentru a continua”
+s.showTranslationButton = "Activate third-party translation"; ”Activează traducerea terță”
+s.hideTranslationButton = "Deactivate Translation"; ”Dezactivează Traducerea”
+s.thirdPartyTranslationDisclaimer = "Translation provided by a third party"; ”Traducerea oferită de o parte terță”
+
+s.notificationsAlreadySubscribed = "You are subscribed to updates for this conversation."; ”Ești abonat la noutăți din această discuție.”
+s.notificationsGetNotified = "Get notified when more statements arrive:"; ”Primește notificări despre noi comentarii”
+s.notificationsEnterEmail = "Enter your email address to get notified when more statements arrive:"; ”Introdu adresa de email pentru a primi notificări despre noi comentarii”
+s.labelEmail = "Email"; ”Email”
+s.notificationsSubscribeButton = "Subscribe";”Abonează-te”
+s.notificationsSubscribeErrorAlert = "Error subscribing";”Eroare abonare”
+s.noCommentsYet = "There aren't any statements yet."; ”Nu există comentarii, deocamdată”
+s.noCommentsYetSoWrite = "Get this conversation started by adding a statement."; ”Pornește discuția printr-un comentariu”
+s.noCommentsYetSoInvite = "Get this conversation started by inviting more participants, or add a statement."; ”Pornește discuția invitând mai mulți participanți sau printr-un comentariu”
+s.noCommentsYouVotedOnAll = "You've voted on all the statements.";”Ai votat la toate comentariile”
+s.noCommentsTryWritingOne = "If you have something to add, try writing your own statement."; ”Dacă vrei să completezi, încearcă să scrii propria afirmație”
+s.convIsClosed = "This conversation is closed."; ”Această discuție este închisă”
+s.noMoreVotingAllowed = "No further voting is allowed."; ”Stop vot”
+
+
+s.topic_good_01 = "What should we do about the pingpong room?"; „Ce ar trebui să facem cu camera de pingpong?”
+s.topic_good_01_reason = "open ended, anyone can have an opinion on answers to this question"; „Întrebare cu răspuns deschis, oricine poate avea o opinie despre răspunsurile la această întrebare”
+s.topic_good_02 = "What do you think about the new proposal?"; „Ce crezi despre noua propunere?”
+s.topic_good_02_reason = "open ended, anyone can have an opinion on answers to this question"; „” „Întrebare cu răspuns deschis, oricine poate avea o opinie despre răspunsurile la această întrebare”
+s.topic_good_03 = "Can you think of anything that's slowing productivity?"; „Ai vreo idee despre cauzele scăderii productivității?”
+
+s.topic_bad_01 = "everyone report your launch readiness"; „Toată lumea raportați gradul de pregătire”
+s.topic_bad_01_reason = "people from various teams will be voting on the responses, but may not have enough knowledge to vote confidently."; „oamenii din echipe diferite vor vota pentru răspunsuri, dar nu neapărat că vor avea suficiente cunoștințe pentru a vota sigur.”
+s.topic_bad_02 = "what are our launch blockers?"; „Ce ne împiedică să începem?”
